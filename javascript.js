76f2b8b00729f6b596efdc8a9c59e8ab4f79531a
Rikko <bulletbill51@yahoo.com>
2023-10-11 14:52:44 -0700

    Added section displaying result of current round

--- javascript.js
+++ javascript.js
@@ -23,37 +23,47 @@ playerRock.addEventListener ("click", displayScore);
 playerPaper.addEventListener("click", displayScore);
 playerScissors.addEventListener("click", displayScore);
 
-
 let playerScore = 0;
 let computerScore = 0;
 const resultsSection = document.querySelector("#gameOver");
+let playButton = document.querySelectorAll(".playbutton");
+
+let gameOver = false;
+
 function displayScore() {
-    if (playerScore === 5 || computerScore === 5 ) {
+    if (gameOver === true) {
+        return;
+    }
+    else if (playerScore === 5 || computerScore === 5 ) {
        let finalResult = document.createElement('div');
        let playAgain = document.createElement('button');
-       playAgain.addEventListener ("click", () => {
+        playAgain.addEventListener ("click", () => {
         playerScore =  0;
         computerScore = 0;
         playerScoreSection.textContent = 0;
         computerScoreSection.textContent = 0;
         finalResult.remove();
         playAgain.remove();
+        gameOver = false;
        });
         if (playerScore === 5) {
-            finalResult.textContent = "Player Win!";
+            finalResult.textContent = "Player Game Win!";
             resultsSection.appendChild(finalResult)
             playAgain.textContent = "Play Again?"
             resultsSection.appendChild(playAgain);
-
+            playButton.disabled = true;
+            gameOver= true;
         }
         else {
-        finalResult.textContent = "Computer Win!"  
+        finalResult.textContent = "Computer Game Win!"  
         resultsSection.appendChild(finalResult)
         playAgain.textContent = "Play Again?"
         resultsSection.appendChild(playAgain);
-        }
-        
+        playButton.disabled = true;
+        gameOver = true;
+        }   
     }
+    else return;
     }
 
 let playerScoreSection = document.querySelector("#playerScore");
@@ -64,38 +74,47 @@ function getComputerChoice() {
     return computerChoices[Math.floor(Math.random() * computerChoices.length)];
 }
 
-
+let currentResult = document.querySelector("#currentResult")
 
 function playRound () {
     const computerSelection = getComputerChoice();
-    if (computerSelection === playerSelection) {
-        result= "Tie!";
-        playerScoreSection.textContent = playerScore;
-        computerScoreSection.textContent = computerScore
-        return console.log((result))
+    if (playerScore === 5 || computerScore === 5) {
+        return;
     }
-    else if (
-        computerSelection === "rock" && playerSelection === "paper" || 
-        computerSelection === "paper" && playerSelection === "scissors"  || 
-        computerSelection ==="scissors" && playerSelection === "rock"
-        )
-    {   playerScore += 1;
-        playerScoreSection.textContent = playerScore;
-        computerScoreSection.textContent = computerScore
-        result= "Player Win!";
-        return console.log(result)
-    }
-    else if (
-        computerSelection === "rock" && playerSelection === "scissors" ||
-        computerSelection === "paper" && playerSelection === "rock" ||
-        computerSelection === "scissors" && playerSelection === "paper"
-        ) {
-            computerScore += 1;
-            computerScoreSection.textContent = computerScore
+    else {
+        if (computerSelection === playerSelection) {
+            result= "Tie!";
+            currentResult.textContent = result;
             playerScoreSection.textContent = playerScore;
-            result = "Computer Win!" ;
+            computerScoreSection.textContent = computerScore
+            return console.log((result))
+        }
+        else if (
+            computerSelection === "rock" && playerSelection === "paper" || 
+            computerSelection === "paper" && playerSelection === "scissors"  || 
+            computerSelection ==="scissors" && playerSelection === "rock"
+            )
+        {   playerScore += 1;
+            playerScoreSection.textContent = playerScore;
+            computerScoreSection.textContent = computerScore
+            result= "Player won the round!";
+            currentResult.textContent=result;
             return console.log(result)
         }
+        else if (
+            computerSelection === "rock" && playerSelection === "scissors" ||
+            computerSelection === "paper" && playerSelection === "rock" ||
+            computerSelection === "scissors" && playerSelection === "paper"
+            ) {
+                computerScore += 1;
+                computerScoreSection.textContent = computerScore
+                playerScoreSection.textContent = playerScore;
+                result = "Computer won the round!" ;
+                currentResult.textContent=result;
+                return console.log(result)
+            }
+        }
+
 }   
 
 
